c9ca4b70924d924de8c8e1a06352622476bc84a1
atilaprieto <elchapapote@hotmail.com>
2020-09-02 12:23:43 +0200

    02.09.2020

--- script.module.urlresolver/lib/urlresolver/plugins/gounlimited.py
+++ script.module.urlresolver/lib/urlresolver/plugins/gounlimited.py
@@ -1,9 +1,6 @@
 """
-    OVERALL CREDIT TO:
-        t0mm0, Eldorado, VOINAGE, BSTRDMKR, tknorris, smokdpi, TheHighway
-
-    urlresolver XBMC Addon
-    Copyright (C) 2011 t0mm0
+    Plugin for UrlResolver
+    Copyright (C) 2020 gujal
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -18,37 +15,27 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-
 import re
-from lib import helpers
-from urlresolver import common
 from urlresolver.resolver import UrlResolver, ResolverError
-
+from urlresolver import common
+from urlresolver.plugins.lib import jsunpack
+from urlresolver.plugins.lib import helpers
 
 class GoUnlimitedResolver(UrlResolver):
     name = "gounlimited.to"
     domains = ['gounlimited.to']
     pattern = r'(?://|\.)(gounlimited\.to)/(?:embed-)?([0-9a-zA-Z]+)'
-    
-    def __init__(self):
-        self.net = common.Net()
-        
+
     def get_media_url(self, host, media_id):
-
+	
         web_url = self.get_url(host, media_id)
-
-        headers = {'User-Agent': common.RAND_UA, 'verifypeer': 'False'}
-
+        headers = {'User-Agent': common.RAND_UA}
         html = self.net.http_GET(web_url, headers=headers).content
-
-        if html:
-
-            html += helpers.get_packed_data(html)
-            source = re.search(r'''sources\s*:\s*\["([^"]+)''', html)
-            if source:
-                return source.group(1) + '&start_seq=$START_NUMBER$' + helpers.append_headers(headers)
-
-        raise ResolverError("Video not found")
-        
+        r = re.search(r"text/javascript'>(eval.*?)\s*</script>", html, re.DOTALL)
+        if r:
+            html = jsunpack.unpack(r.group(1))
+            sources = helpers.scrape_sources(html)
+            return helpers.pick_source(sources) + helpers.append_headers(headers)
+        raise ResolverError('Video cannot be located.')
     def get_url(self, host, media_id):
         return self._default_get_url(host, media_id, template='https://{host}/embed-{media_id}.html')
